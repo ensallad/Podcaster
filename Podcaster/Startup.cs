32ffc7241830abac6cbb345c82ee13be1120db7f
ensallad <joohan91@hotmail.com>
2017-11-22 19:53:29 +0100

    teststat

---
+++ Podcaster/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Podcaster.Startup))]
+namespace Podcaster
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
