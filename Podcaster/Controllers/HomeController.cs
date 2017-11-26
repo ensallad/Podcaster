78b0d4800a7e890f32ed502b6d5318b71646e838
ensallad <joohan91@hotmail.com>
2017-11-26 21:32:21 +0100

    Konstigt, men hoppas det fungerar på bärbara med felsöknignen

--- Podcaster/Controllers/HomeController.cs
+++ Podcaster/Controllers/HomeController.cs
@@ -15,6 +15,7 @@ namespace Podcaster.Controllers
 {
     public class HomeController : Controller
     {
+       
         public ActionResult Index()
        {
             return View();
@@ -22,24 +23,12 @@ namespace Podcaster.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
-        public ActionResult PlayingPage()
-        {
             ViewBag.Message = "Your Podcast playing page.";
 
+            string PodcastName = "felfelfel";
+
             List<PodcastEpisodeModel> podEpisode = new List<PodcastEpisodeModel>();
-         
+
             XmlDocument xmlDoc = new XmlDocument();
             xmlDoc.Load("http://deadlysiriussxm.libsyn.com/rss");
 
@@ -47,12 +36,16 @@ namespace Podcaster.Controllers
             {
                 var mainTitle = temp.SelectSingleNode("title").InnerText;
 
+                PodcastName = mainTitle;
                 podEpisode.Add(new PodcastEpisodeModel
                 {
                     Description = mainTitle
+
                 });
 
             }
+            ViewBag.PodcastName = PodcastName;
+
             foreach (System.Xml.XmlNode temp in xmlDoc.DocumentElement.SelectNodes("channel/item"))
             {
                 var mainTitle = temp.SelectSingleNode("title").InnerText; ;
@@ -93,48 +86,90 @@ namespace Podcaster.Controllers
             return View(podEpisode);
         }
 
-
-        public ActionResult GetPodEpisodes()
+        public ActionResult Contact()
         {
-            //object model = null;
+            ViewBag.Message = "Your contact page.";
 
-            //var podName = "";
+            return View();
+        }
 
-            //if (viewName == "CustomerDetails")
-            //{
-            //    using (NorthwindEntities db = new NorthwindEntities())
-            //    {
-            //        model = db.Customers.Find(customerID);
-            //    }
-            //}
-            //if (viewName == "OrderDetails")
-            //{
-            //    using (NorthwindEntities db = new NorthwindEntities())
-            //    {
-            //        model = db.Orders.Where(o => o.CustomerID == customerID)
-            //                  .OrderBy(o => o.OrderID).ToList();
-            //    }
-            //}
 
-            //List<PodcastEpisodeModel> podEpisode = new List<PodcastEpisodeModel>();
 
-            //XmlDocument xmlDoc = new XmlDocument();
-            // xmlDoc.Load("http://deadlysiriussxm.libsyn.com/rss");
+        public ActionResult PlayingPage(string rssFeed)
+        {
+            string PodcastName = "felfelfel";
 
-            //var document = GetXDocFromPodcast();
-            //XElement SingleNode = (from c in xmlDoc.
-            //                       Descendants("Feed")
-            //                       where c.Element("feedName").Value == podname
-            //                       select c).FirstOrDefault();
+            List<PodcastEpisodeModel> podCast = new List<PodcastEpisodeModel>();
 
-            //var mainTitle = xmlDoc.SelectSingleNode("title");
-            //var main = mainTitle.InnerText;
-            //podName = main;
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(rssFeed);
+            foreach (System.Xml.XmlNode temp in xmlDoc.DocumentElement.SelectNodes("channel"))
+            {
+                var mainTitle = temp.SelectSingleNode("title").InnerText;
 
-            //model = main;
-            //return PartialView("PlayingPage", podName);
+                PodcastName = mainTitle;
+                podCast.Add(new PodcastEpisodeModel
+                {
+                    Description = mainTitle
 
-            return View("Index");
+                });
+
+            }
+            ViewBag.PodcastName = PodcastName;
+
+            foreach (System.Xml.XmlNode temp in xmlDoc.DocumentElement.SelectNodes("channel/item"))
+            {
+                var mainTitle = temp.SelectSingleNode("title").InnerText; ;
+                //var main = mainTitle.InnerText;
+
+                var PubDt = temp.SelectSingleNode("pubDate").InnerText;
+
+                var epiUrl = "";
+                var enclosure = temp.SelectSingleNode("enclosure");
+                if (enclosure != null) { epiUrl = enclosure.Attributes["url"].Value; }
+                var media = temp.SelectSingleNode("media");
+                if (media != null)
+                { epiUrl = media.Attributes["url"].Value; }
+
+                string desc = null;
+                var description = temp.SelectSingleNode("description");
+                if (description != null)
+                {
+                    desc = description.InnerText;
+                }
+                else
+                {
+                    desc = null;
+                }
+
+                podCast.Add(new PodcastEpisodeModel
+                {
+                    episodeTitle = mainTitle,
+                    PublicationDate = PubDt,
+                    episodeUrl = epiUrl,
+                    Description = desc
+                });
+
+
+            }
+            ViewBag.ListEpisode = podCast;
+
+            //get the latest episodeUrl to the player in playingpage
+            var firstItem = podCast[0]; 
+            var firstUrl = firstItem.PublicationDate;
+
+            ViewBag.LatestUrl = firstUrl;
+
+            return View(podCast);
+        }
+
+
+        public ActionResult GetPodEpisodes(string podRss)
+        {
+
+            return View();
+
+            //return PartialView("podResults", podEpisodeTitle);
         }
 
 
